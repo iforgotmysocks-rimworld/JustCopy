3481854498c66fa386a49abeda42cbca1564b700
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-07-18 23:36:25 +0200

    added button to set restriction as default

--- Source/JustCopy/JustCopy/HarmonyPatcher.cs
+++ Source/JustCopy/JustCopy/HarmonyPatcher.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using Verse;
+using Verse.Sound;
 
 namespace JustCopy
 {
@@ -22,9 +23,13 @@ namespace JustCopy
     public class Dialog_ManageFoodRestrictions_DoWindowContetents
     {
         public static readonly float RestrictionOffset = 450f + 30f;
+        public static readonly float RestrictionWidth = 85f;
+        public static readonly float DefaultOffset = RestrictionOffset + RestrictionWidth + 10f;
+        public static readonly float DefaultWidth = 77f;
+
         public static void Postfix(Dialog_ManageFoodRestrictions __instance)
         {
-            var copyRect = new Rect(RestrictionOffset, 0f, 150f, 35f);
+            var copyRect = new Rect(RestrictionOffset, 0f, RestrictionWidth, 35f);
             if (Widgets.ButtonText(copyRect, "JC_NewFromExisting".Translate(), true, true, true))
             {
                 var floatMenuList = new List<FloatMenuOption>();
@@ -46,6 +51,22 @@ namespace JustCopy
                 }
                 Find.WindowStack.Add(new FloatMenu(floatMenuList));
             }
+
+            var defaultRect = new Rect(Dialog_ManageFoodRestrictions_DoWindowContetents.DefaultOffset, 0f, Dialog_ManageFoodRestrictions_DoWindowContetents.DefaultWidth, 35f);
+            var currentRest = typeof(Dialog_ManageFoodRestrictions).GetField("selFoodRestrictionInt", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance) as FoodRestriction;
+            if (currentRest == null) return;
+            if (Widgets.ButtonText(defaultRect, "JC_Default".Translate(), true, true, true))
+            {
+                var restList = Current.Game.foodRestrictionDatabase.AllFoodRestrictions;
+                if (restList.IndexOf(currentRest) == 0)
+                {
+                    SoundDefOf.ClickReject.PlayOneShot(null);
+                    return;
+                }
+                restList.Remove(currentRest);
+                restList.Insert(0, currentRest);
+                SoundDefOf.Click.PlayOneShot(null);
+            }
         }
     }
 
@@ -54,7 +75,7 @@ namespace JustCopy
     {
         public static void Postfix(Dialog_ManageOutfits __instance)
         {
-            var copyRect = new Rect(Dialog_ManageFoodRestrictions_DoWindowContetents.RestrictionOffset, 0f, 150f, 35f);
+            var copyRect = new Rect(Dialog_ManageFoodRestrictions_DoWindowContetents.RestrictionOffset, 0f, Dialog_ManageFoodRestrictions_DoWindowContetents.RestrictionWidth, 35f);
             if (Widgets.ButtonText(copyRect, "JC_NewFromExisting".Translate(), true, true, true))
             {
                 var floatMenuList = new List<FloatMenuOption>();
@@ -76,6 +97,22 @@ namespace JustCopy
                 }
                 Find.WindowStack.Add(new FloatMenu(floatMenuList));
             }
+
+            var defaultRect = new Rect(Dialog_ManageFoodRestrictions_DoWindowContetents.DefaultOffset, 0f, Dialog_ManageFoodRestrictions_DoWindowContetents.DefaultWidth, 35f);
+            var currentRest = typeof(Dialog_ManageOutfits).GetField("selOutfitInt", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance) as Outfit;
+            if (currentRest == null) return;
+            if (Widgets.ButtonText(defaultRect, "JC_Default".Translate(), true, true, true))
+            {
+                var restList = Current.Game.outfitDatabase.AllOutfits;
+                if (restList.IndexOf(currentRest) == 0)
+                {
+                    SoundDefOf.ClickReject.PlayOneShot(null);
+                    return;
+                }
+                restList.Remove(currentRest);
+                restList.Insert(0, currentRest);
+                SoundDefOf.Click.PlayOneShot(null);
+            }
         }
     }
 
@@ -84,7 +121,7 @@ namespace JustCopy
     {
         public static void Postfix(Dialog_ManageDrugPolicies __instance)
         {
-            var copyRect = new Rect(Dialog_ManageFoodRestrictions_DoWindowContetents.RestrictionOffset, 0f, 150f, 35f);
+            var copyRect = new Rect(Dialog_ManageFoodRestrictions_DoWindowContetents.RestrictionOffset, 0f, Dialog_ManageFoodRestrictions_DoWindowContetents.RestrictionWidth, 35f);
             if (Widgets.ButtonText(copyRect, "JC_NewFromExisting".Translate(), true, true, true))
             {
                 var floatMenuList = new List<FloatMenuOption>();
@@ -114,6 +151,22 @@ namespace JustCopy
                     }, MenuOptionPriority.Default, null, null, 0f, null, null));
                 }
                 Find.WindowStack.Add(new FloatMenu(floatMenuList));
+            }
+
+            var defaultRect = new Rect(Dialog_ManageFoodRestrictions_DoWindowContetents.DefaultOffset, 0f, Dialog_ManageFoodRestrictions_DoWindowContetents.DefaultWidth, 35f);
+            var currentRest = typeof(Dialog_ManageDrugPolicies).GetField("selPolicy", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance) as DrugPolicy;
+            if (currentRest == null) return;
+            if (Widgets.ButtonText(defaultRect, "JC_Default".Translate(), true, true, true))
+            {
+                var restList = Current.Game.drugPolicyDatabase.AllPolicies;
+                if (restList.IndexOf(currentRest) == 0)
+                {
+                    SoundDefOf.ClickReject.PlayOneShot(null);
+                    return;
+                }
+                restList.Remove(currentRest);
+                restList.Insert(0, currentRest);
+                SoundDefOf.Click.PlayOneShot(null);
             }
         }
     }
